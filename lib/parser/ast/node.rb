6d51847d36db259f39a886c61531d37718e5adeb
Peter Zotov <whitequark@whitequark.org>
2013-04-21 19:39:48 +0400

    Specialize Parser::AST::Node#assign_properties.

--- lib/parser/ast/node.rb
+++ lib/parser/ast/node.rb
@@ -5,6 +5,12 @@ module Parser
       attr_reader :source_map
 
       alias src source_map
+
+      def assign_properties(properties)
+        if (source_map = properties[:source_map])
+          @source_map = source_map
+        end
+      end
     end
 
   end
