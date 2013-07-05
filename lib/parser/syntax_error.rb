b1af11b81bccebc2c4ccf9732b33e50dd0f6db53
Yorick Peterse <yorickpeterse@gmail.com>
2013-07-05 19:27:38 +0200

    "Documentation" for Parser::SyntaxError.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- lib/parser/syntax_error.rb
+++ lib/parser/syntax_error.rb
@@ -1,3 +1,7 @@
 module Parser
+  ##
+  # {Parser::SyntaxError} is raised whenever parser detects a syntax error
+  # (what a surprise!) similar to the standard SyntaxError class.
+  #
   class SyntaxError < StandardError; end
 end
