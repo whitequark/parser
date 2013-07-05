b6ffbbbc86e5adceabe057f7475dae4bec69f2bb
Yorick Peterse <yorickpeterse@gmail.com>
2013-07-05 19:30:49 +0200

    Basic documentation for Parser::AST::Node.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- lib/parser/ast/node.rb
+++ lib/parser/ast/node.rb
@@ -1,11 +1,28 @@
 module Parser
   module AST
 
+    ##
+    # {Parser::AST::Node} contains information about a single AST node and its
+    # child nodes, it extends the basic `AST::Node` class provided by the "ast"
+    # Gem.
+    #
+    # @!attribute [r] location
+    #  @return [Parser::Source::Map]
+    #
     class Node < ::AST::Node
       attr_reader :location
 
       alias loc location
 
+      ##
+      # Assigns various properties to the current AST node. Currently only the
+      # location can be set.
+      #
+      # @param [Hash] properties
+      #
+      # @option properties [Parser::Source::Map] :location Location information
+      #  of the node.
+      #
       def assign_properties(properties)
         if (location = properties[:location])
           @location = location
