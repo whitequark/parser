314e8c278f6f99901a389823ca4244694f7affe6
Peter Zotov <whitequark@whitequark.org>
2014-02-24 12:30:31 +0400

    - Parser::Base, ruby18.y: don't try to lookup Encoding on 1.8 (fixes #133).

---
+++ test/test_base.rb
@@ -0,0 +1,18 @@
+require 'helper'
+require 'parser/current'
+
+class TestBase < Minitest::Test
+  include AST::Sexp
+
+  def test_parse
+    ast = Parser::CurrentRuby.parse('1')
+    assert_equal s(:int, 1), ast
+  end
+
+  def test_parse_with_comments
+    ast, comments = Parser::CurrentRuby.parse_with_comments('1 # foo')
+    assert_equal s(:int, 1), ast
+    assert_equal 1, comments.size
+    assert_equal '# foo', comments.first.text
+  end
+end
