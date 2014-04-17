2eb3b43d23d7d24f5b9b059701f9f22de969e49d
Peter Zotov <whitequark@whitequark.org>
2014-04-17 07:40:37 +0400

    test/test_current: extend to 2.1.1.

--- test/test_current.rb
+++ test/test_current.rb
@@ -10,7 +10,7 @@ class TestCurrent < Minitest::Test
       assert_equal Parser::Ruby19, Parser::CurrentRuby
     when '2.0.0'
       assert_equal Parser::Ruby20, Parser::CurrentRuby
-    when '2.1.0'
+    when '2.1.0', '2.1.1'
       assert_equal Parser::Ruby21, Parser::CurrentRuby
     else
       flunk "Update test_parser_current for #{RUBY_VERSION}"
