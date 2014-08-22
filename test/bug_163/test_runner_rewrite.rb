b621b2bc8830982d970bdc75ebbcd95cb96dc5b0
Jon Frisby <jfrisby@mrjoy.com>
2014-08-22 14:36:13 -0700

    Use `Dir.mktmpdir` instead of `FileUtils.mkdir`.

--- test/bug_163/test_runner_rewrite.rb
+++ test/bug_163/test_runner_rewrite.rb
@@ -8,27 +8,28 @@ require (BASE_DIR + 'helper').expand_path
 class TestRunnerRewrite < Minitest::Test
   def setup
     @ruby_rewrite = BASE_DIR.expand_path + '../bin/ruby-rewrite'
-    @tmp_dir      = BASE_DIR + 'tmp'
     @test_dir     = BASE_DIR + 'bug_163'
     @fixtures_dir = @test_dir + 'fixtures'
-    FileUtils.mkdir_p(@tmp_dir)
   end
 
   def test_rewriter
-    sample_file = @tmp_dir + 'bug_163.rb'
-    sample_file_expanded = sample_file.expand_path
-    expected_file = @fixtures_dir + 'output.rb'
+    Dir.mktmpdir(nil, BASE_DIR.expand_path.to_s) do |tmp_dir|
+      tmp_dir = Pathname.new(tmp_dir)
+      sample_file = tmp_dir + 'bug_163.rb'
+      sample_file_expanded = sample_file.expand_path
+      expected_file = @fixtures_dir + 'output.rb'
 
-    FileUtils.cp(@fixtures_dir + 'input.rb', @tmp_dir + 'bug_163.rb')
-    FileUtils.cd @test_dir do
-      exit_code = system %Q{
-        #{Shellwords.escape(@ruby_rewrite.to_s)} --modify \
-          -l rewriter.rb \
-          #{Shellwords.escape(sample_file_expanded.to_s)}
-      }
+      FileUtils.cp(@fixtures_dir + 'input.rb', tmp_dir + 'bug_163.rb')
+      FileUtils.cd @test_dir do
+        exit_code = system %Q{
+          #{Shellwords.escape(@ruby_rewrite.to_s)} --modify \
+            -l rewriter.rb \
+            #{Shellwords.escape(sample_file_expanded.to_s)}
+        }
+      end
+
+      assert File.read(expected_file.expand_path) == File.read(sample_file),
+        "#{sample_file} should be identical to #{expected_file}"
     end
-
-    assert File.read(expected_file.expand_path) == File.read(sample_file),
-      "#{sample_file} should be identical to #{expected_file}"
   end
 end
