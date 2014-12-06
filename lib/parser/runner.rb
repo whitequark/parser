e97da2fea5984b551f46dca1252581c4f19926c7
Brett G. Giles <brett.giles@drogar.com>
2014-12-05 20:26:32 -0700

    Forgot the require_relative in runner.rb

--- lib/parser/runner.rb
+++ lib/parser/runner.rb
@@ -2,7 +2,11 @@ require 'benchmark'
 require 'find'
 require 'slop'
 
-require_relative './../parser'
+if respond_to? :require_relative
+  require_relative './../parser'
+else
+  require 'parser'
+end
 
 module Parser
 
