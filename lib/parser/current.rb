e93b9080f58fe57c217b0c3f16cb61ed1ad45d73
whitequark <whitequark@whitequark.org>
2015-04-30 14:17:01 +0300

    * parser/current: bump warnings to 2.0.1, 2.1.7, 2.2.3.
    This means master can only be released as a gem once all
    the Ruby branches above are released.

--- lib/parser/current.rb
+++ lib/parser/current.rb
@@ -25,24 +25,32 @@ module Parser
     CurrentRuby = Ruby19
 
   when /^2\.0\./
+    if RUBY_VERSION != '2.0.1'
+      warn_syntax_deviation 'parser/ruby20', '2.0.1'
+    end
+
     require 'parser/ruby20'
     CurrentRuby = Ruby20
 
   when /^2\.1\./
-    if RUBY_VERSION != '2.1.6'
-      warn_syntax_deviation 'parser/ruby21', '2.1.6'
+    if RUBY_VERSION != '2.1.7'
+      warn_syntax_deviation 'parser/ruby21', '2.1.7'
     end
 
     require 'parser/ruby21'
     CurrentRuby = Ruby21
 
   when /^2\.2\./
+    if RUBY_VERSION != '2.2.3'
+      warn_syntax_deviation 'parser/ruby22', '2.2.3'
+    end
+
     require 'parser/ruby22'
     CurrentRuby = Ruby22
 
   else # :nocov:
     # Keep this in sync with released Ruby.
-    warn_syntax_deviation 'parser/ruby22', '2.2'
+    warn_syntax_deviation 'parser/ruby22', '2.2.x'
     require 'parser/ruby22'
     CurrentRuby = Ruby22
   end
