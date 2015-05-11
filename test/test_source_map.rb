whitequark/parser

commit 3883e3d43802db73ab1de03cced15ed6ba254fe6

+ Add Parser::Source::Map#node.
The #node method allows to retrieve the node that the map is
attached to. It is also now guaranteed that one source map is
attached to exactly one node. Both of these changes make it easier
to work with Parser::Source::Comment.associate_locations.

lib/parser/source/map.rb
@@ -62,14 +62,21 @@ module Parser
     # @api public
     #
     class Map
+      attr_reader :node
       attr_reader :expression
 
       ##
       # @param [Range] expression
       def initialize(expression)
         @expression = expression
+      end
 
+      ##
+      # @api private
+      def node=(node)
+        @node = node
         freeze
+        @node
       end
 
       ##
@@ -134,7 +141,7 @@ module Parser
       protected
 
       def with(&block)
-        dup.tap(&block).freeze
+        dup.tap(&block)
       end
 
       def update_expression(expression_l)

lib/parser/ast/node.rb
@@ -27,6 +27,7 @@ module Parser
       #
       def assign_properties(properties)
         if (location = properties[:location])
+          location.node = self
           @location = location
         end
       end

lib/parser/builders/default.rb
@@ -332,7 +332,7 @@ module Parser
       when :__FILE__
         if @emit_file_line_as_literals
           n(:str, [ node.loc.expression.source_buffer.name ],
-            node.loc)
+            node.loc.dup)
         else
           node
         end
@@ -340,14 +340,14 @@ module Parser
       when :__LINE__
         if @emit_file_line_as_literals
           n(:int, [ node.loc.expression.line ],
-            node.loc)
+            node.loc.dup)
         else
           node
         end
 
       when :__ENCODING__
         n(:const, [ n(:const, [ nil, :Encoding], nil), :UTF_8 ],
-          node.loc)
+          node.loc.dup)
 
       when :ident
         name, = *node
@@ -594,7 +594,7 @@ module Parser
         expr.updated(:arg)
       else
         n(:arg_expr, [ expr ],
-          expr.loc)
+          expr.loc.dup)
       end
     end
 
@@ -605,7 +605,7 @@ module Parser
         expr.updated(:restarg)
       else
         n(:restarg_expr, [ expr ],
-          expr.loc)
+          expr.loc.dup)
       end
     end
 
@@ -614,7 +614,7 @@ module Parser
         expr.updated(:blockarg)
       else
         n(:blockarg_expr, [ expr ],
-          expr.loc)
+          expr.loc.dup)
       end
     end
 

test/parse_helper.rb
@@ -95,6 +95,8 @@ module ParseHelper
           raise "No entity with AST path #{ast_path} in #{parsed_ast.inspect}"
         end
 
+        assert astlet.frozen?
+
         assert astlet.location.respond_to?(map_field),
                "(#{version}) #{astlet.location.inspect}.respond_to?(#{map_field.inspect}) for:\n#{parsed_ast.inspect}"
 

test/test_base.rb
@@ -15,4 +15,9 @@ class TestBase < Minitest::Test
     assert_equal 1, comments.size
     assert_equal '# foo', comments.first.text
   end
+
+  def test_loc_to_node
+    ast = Parser::CurrentRuby.parse('1')
+    assert_equal ast.loc.node, ast
+  end
 end

commit 3841690924903bc4bf3c078e8556314ffc4c52d7

- Source::Map: do not include :node in to_hash.
This fixes a regression introduced in 3883e3d, and makes
ruby-parse -L work again.

lib/parser/source/map.rb
@@ -144,9 +144,11 @@ module Parser
       # @return [Hash(Symbol, Parser::Source::Range)]
       #
       def to_hash
-        Hash[instance_variables.map do |ivar|
-          [ ivar[1..-1].to_sym, instance_variable_get(ivar) ]
-        end]
+        instance_variables.inject({}) do |hash, ivar|
+          next hash if ivar == :@node
+          hash[ivar[1..-1].to_sym] = instance_variable_get(ivar)
+          hash
+        end
       end
 
       protected

test/test_source_map.rb
@@ -1,0 +1,13 @@
+require 'helper'
+require 'parse_helper'
+
+class TestSourceComment < Minitest::Test
+  include ParseHelper
+
+  def test_to_hash
+    buf = Parser::Source::Buffer.new("<input>")
+    buf.source = "1"
+    ast = parser_for_ruby_version('1.8').parse(buf)
+    assert_equal [:operator, :expression], ast.loc.to_hash.keys
+  end
+end