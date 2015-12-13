93f1130692827ea53cc7a06745f9b9716f589f37
Alex Dowad <alexinbeijing@gmail.com>
2015-12-13 20:35:48 +0200

    Rewriter merges actions when possible, avoiding unnecessary 'clobber' errors
    If 2 rewrite actions both attempt to delete the same piece of text, there is
    no reason to raise a 'clobber' exception. Likewise, if they both replace the
    same text with the same replacement, that is fine.
    
    Even if they replace 2 overlapping regions of text, that is still fine IF
    the overlapping portion of the replacement is the same.

--- lib/parser/source/rewriter.rb
+++ lib/parser/source/rewriter.rb
@@ -159,25 +159,10 @@ module Parser
       private
 
       def append(action)
-        if (clobber_action = clobbered?(action.range))
-          # cannot replace 3 characters with "foobar"
-          diagnostic = Diagnostic.new(:error,
-                                      :invalid_action,
-                                      { :action => action },
-                                      action.range)
-          @diagnostics.process(diagnostic)
-
-          # clobbered by: remove 3 characters
-          diagnostic = Diagnostic.new(:note,
-                                      :clobbered,
-                                      { :action => clobber_action },
-                                      clobber_action.range)
-          @diagnostics.process(diagnostic)
-
-          raise ClobberingError, "Parser::Source::Rewriter detected clobbering"
+        if (clobber_actions = clobbered?(action.range))
+          handle_clobber(action, clobber_actions)
         else
           clobber(action.range)
-
           active_queue << action
         end
 
@@ -190,10 +175,77 @@ module Parser
 
       def clobbered?(range)
         if active_clobber & ((2 ** range.size - 1) << range.begin_pos) != 0
-          active_queue.find do |action|
-            action.range.to_a & range.to_a
+          active_queue.select do |action|
+            action.range.end_pos > range.begin_pos &&
+              range.end_pos > action.range.begin_pos
           end
         end
+      end
+
+      def handle_clobber(action, existing)
+        if can_merge?(action, existing)
+          merge_actions!(action, existing)
+        else
+          # cannot replace 3 characters with "foobar"
+          diagnostic = Diagnostic.new(:error,
+                                      :invalid_action,
+                                      { :action => action },
+                                      action.range)
+          @diagnostics.process(diagnostic)
+
+          # clobbered by: remove 3 characters
+          diagnostic = Diagnostic.new(:note,
+                                      :clobbered,
+                                      { :action => existing[0] },
+                                      existing[0].range)
+          @diagnostics.process(diagnostic)
+
+          raise ClobberingError, "Parser::Source::Rewriter detected clobbering"
+        end
+      end
+
+      def can_merge?(action, existing)
+        existing.all? do |other|
+          overlap       = action.range.intersect(other.range)
+          action_offset = overlap.begin_pos - action.range.begin_pos
+          other_offset  = overlap.begin_pos - other.range.begin_pos
+
+          replacement1 = action.replacement[action_offset, overlap.size] || ''
+          replacement2 = other.replacement[other_offset, overlap.size] || ''
+          replacement1 == replacement2
+        end
+      end
+
+      def merge_actions!(action, existing)
+        actions      = existing.push(action).sort_by { |a| a.range.begin_pos }
+        merged_begin = actions.map { |act| act.range.begin_pos }.min
+        merged_end   = actions.map { |act| act.range.end_pos }.max
+        range        = Source::Range.new(@source_buffer,
+                                         merged_begin,
+                                         merged_end)
+        clobber(range)
+
+        replacement = merge_replacements(actions)
+        replace_actions(actions, Rewriter::Action.new(range, replacement))
+      end
+
+      def replace_actions(old, updated)
+        old.each { |act| active_queue.delete(act) }
+        active_queue << updated
+      end
+
+      def merge_replacements(actions)
+        # `actions` must be sorted by beginning position
+        begin_pos = actions.first.range.begin_pos
+        result    = ''
+
+        actions.each do |act|
+          offset = result.size - act.range.begin_pos + begin_pos
+          next if offset < 0 || offset >= act.replacement.size
+          result << act.replacement[offset..-1]
+        end
+
+        result
       end
 
       def in_transaction?
