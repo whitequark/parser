b797293999b0d022e4ee0597ea5a5d2140e18f6a
Marc-Andre Lafortune <github@marc-andre.ca>
2017-12-04 14:40:30 -0500

    Extract assert_rewriter_output

--- test/test_runner_rewrite.rb
+++ test/test_runner_rewrite.rb
@@ -6,30 +6,31 @@ BASE_DIR = Pathname.new(__FILE__) + '..'
 require (BASE_DIR + 'helper').expand_path
 
 class TestRunnerRewrite < Minitest::Test
-  def setup
+  def assert_rewriter_output(path, args, input: 'input.rb', output: 'output.rb')
     @ruby_rewrite = BASE_DIR.expand_path + '../bin/ruby-rewrite'
-    @test_dir     = BASE_DIR + 'bug_163'
+    @test_dir     = BASE_DIR + path
     @fixtures_dir = @test_dir + 'fixtures'
-  end
 
-  def test_rewriter
     Dir.mktmpdir("parser", BASE_DIR.expand_path.to_s) do |tmp_dir|
       tmp_dir = Pathname.new(tmp_dir)
-      sample_file = tmp_dir + 'bug_163.rb'
+      sample_file = tmp_dir + "#{path}.rb"
       sample_file_expanded = sample_file.expand_path
-      expected_file = @fixtures_dir + 'output.rb'
+      expected_file = @fixtures_dir + output
 
-      FileUtils.cp(@fixtures_dir + 'input.rb', tmp_dir + 'bug_163.rb')
+      FileUtils.cp(@fixtures_dir + input, sample_file_expanded)
       FileUtils.cd @test_dir do
         exit_code = system %Q{
-          #{Shellwords.escape(@ruby_rewrite.to_s)} --modify \
-            -l rewriter.rb \
-            #{Shellwords.escape(sample_file_expanded.to_s)}
+          #{Shellwords.escape(@ruby_rewrite.to_s)} #{args} \
+          #{Shellwords.escape(sample_file_expanded.to_s)}
         }
       end
 
       assert File.read(expected_file.expand_path) == File.read(sample_file),
         "#{sample_file} should be identical to #{expected_file}"
     end
+  end
+
+  def test_rewriter_bug_163
+    assert_rewriter_output('bug_163', '--modify  -l rewriter.rb')
   end
 end
