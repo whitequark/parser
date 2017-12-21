b4da1e343ff71106dd0c44a963de6919d551b501
Marc-Andre Lafortune <github@marc-andre.ca>
2017-12-21 15:19:59 -0500

    + Add Parser::TreeRewriter

---
+++ test/using_tree_rewriter/using_tree_rewriter.rb
@@ -0,0 +1,7 @@
+class UsingTreeRewriter < Parser::TreeRewriter
+  def on_send(node)
+    wrap(node.loc.expression, '(', ')')
+    wrap(node.children[0].loc.expression, '[', ']')
+    super
+  end
+end
