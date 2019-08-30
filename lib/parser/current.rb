4da18918a70df941769c4200753e48cc4389c323
Koichi ITO <koic.ito@gmail.com>
2019-08-30 12:55:39 +0900

    Bump maintenance branches to 2.6.3, 2.5.6, and 2.4.7 (#598)
    * Bump 2.6 branch to 2.6.4
    
    Ruby 2.6.4 has been released.
    https://www.ruby-lang.org/en/news/2019/08/28/ruby-2-6-4-released/
    
    There seems to be no change to the syntax.
    https://github.com/ruby/ruby/compare/v2_6_3...v2_6_4
    
    * Bump 2.5 branch to 2.5.6
    
    Ruby 2.5.6 has been released.
    https://www.ruby-lang.org/en/news/2019/08/28/ruby-2-5-6-released/
    
    ruby/ruby@764fe09 seems to be the only syntax change from Ruby 2.5.5 to Ruby 2.5.6.
    https://github.com/ruby/ruby/compare/v2_5_5...v2_5_6
    
    * + ruby25.y: dsym should be treated as string
    
    This PR tracks upstream commit ruby/ruby@764fe09.
    
    `xstring_contents` and `string_contents` rules are equivalent, so it doesn't
    change anything for parser.
    
    * Bump 2.4 branch to 2.4.7
    
    Ruby 2.4.7 has been released.
    https://www.ruby-lang.org/en/news/2019/08/28/ruby-2-4-7-released/
    
    There seems to be no change to the syntax.
    https://github.com/ruby/ruby/compare/v2_4_6...v2_4_7

--- lib/parser/current.rb
+++ lib/parser/current.rb
@@ -48,7 +48,7 @@ module Parser
     CurrentRuby = Ruby23
 
   when /^2\.4\./
-    current_version = '2.4.6'
+    current_version = '2.4.7'
     if RUBY_VERSION != current_version
       warn_syntax_deviation 'parser/ruby24', current_version
     end
@@ -57,7 +57,7 @@ module Parser
     CurrentRuby = Ruby24
 
   when /^2\.5\./
-    current_version = '2.5.5'
+    current_version = '2.5.6'
     if RUBY_VERSION != current_version
       warn_syntax_deviation 'parser/ruby25', current_version
     end
@@ -66,7 +66,7 @@ module Parser
     CurrentRuby = Ruby25
 
   when /^2\.6\./
-    current_version = '2.6.3'
+    current_version = '2.6.4'
     if RUBY_VERSION != current_version
       warn_syntax_deviation 'parser/ruby26', current_version
     end
