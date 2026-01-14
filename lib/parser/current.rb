04b1b3baf6ac89283f7fd99af5eb3f3bc05c4054
Koichi ITO <koic.ito@gmail.com>
2026-01-14 11:35:26 +0900

    * Bump maintenance branches to 3.2.10
    Ruby 3.2.10 has been released:
    https://www.ruby-lang.org/en/news/2026/01/14/ruby-3-2-10-released/
    
    Bump 3.2 branch from 3.2.9 to 3.2.10:
    https://github.com/ruby/ruby/compare/v3_2_9...v3_2_10
    
    There seems to be no change to syntax.

--- lib/parser/current.rb
+++ lib/parser/current.rb
@@ -102,7 +102,7 @@ module Parser
     CurrentRuby = Ruby31
 
   when /^3\.2\./
-    current_version = '3.2.9'
+    current_version = '3.2.10'
     if RUBY_VERSION != current_version
       warn_syntax_deviation 'parser/ruby32', current_version
     end
